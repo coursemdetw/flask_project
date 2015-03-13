b568744c7c04d0517db3d1430967f266a898b86d
bringlinyen <bringlinyen@gmail.com>
2015-03-13 14:34:42 +0800

    test openshift and localhost at one wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
-
-from myflaskapp import app as application
+import os
+import myflaskapp
+#from myflaskapp import app as application
+if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
+    # 表示程式在雲端執行
+    application = myflaskapp.app
+else:
+    # 表示在近端執行
+    myflaskapp.app.run()
+    
 
