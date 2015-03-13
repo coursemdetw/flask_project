c5f247c039fa202de93be8f694048ef446d70993
coursemdetw <coursemdetw@gmail.com>
2015-03-13 16:29:11 +0800

    update leo

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='KMOL Flask project',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='KMOL',
+      author_email='test@mde.tw',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       install_requires=['Flask>=0.10.1', 'MarkupSafe'],
      )
